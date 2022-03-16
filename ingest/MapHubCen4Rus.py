c56cfd7de3b701d8db8662b8c7f6636fdeba5c31
habitual <habitual@habitual-media.com>
2022-03-16 13:44:51 -0400

    MapHubCen4Rus test. Still needs entry deduplication.

---
+++ ingest/MapHubCen4Rus.py
@@ -0,0 +1,39 @@
+import time
+from os.path import exists
+import redis
+import manager.config
+import requests
+
+
+def get_points():
+    return requests.post("https://maphub.net/json/map_load/176607","{}").json()["geojson"]["features"]
+
+
+class QueryClass:
+    def __init__(self):
+        self.data = ""
+        self.QueryTime = 10
+        self.n = self.QueryTime
+        parsed_yaml = manager.config.get_config()
+        while True:
+            if self.n >= self.QueryTime:
+                self.n = 0
+                self.load_redis(parsed_yaml)
+            time.sleep(1)
+            self.n += 1
+            if not exists("ingest.Example.lck"):
+                break
+
+    def load_redis(self, parsed_yaml):
+        redis_host = parsed_yaml['Databases']['Redis']['Host']
+        redis_port = parsed_yaml['Databases']['Redis']['Port']
+        r = redis.Redis(host=redis_host, port=redis_port)
+        self.retrieve_data()
+        for record in self.data:
+            try:
+                r.rpush("data", '{"Module":"MapHubCen4Rus", "Data": ' + str(record).replace("\'", "\"")[:-1] + ",\"TimeStamp\":\"" + str(time.time()) + '"}}')
+            except:
+                print("PowerOutageUS : Unable to push to Redis stack")
+
+    def retrieve_data(self):
+        self.data = '{"Module":"Example", "Data": {"name":"John", "age":30, "car":"civic"}}'
