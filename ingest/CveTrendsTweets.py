355a924ec845097140f88a85703e4c906c26b7d1
habitual <habitual@habitual-media.com>
2022-03-25 02:21:44 -0400

    Testing CveTrends module

--- ingest/CveTrendsTweets.py
+++ ingest/CveTrendsTweets.py
@@ -11,20 +11,16 @@ import ast
 
 
 def get_points():
-    entry_list = []
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
-    raw_json = requests.post("https://maphub.net/json/map_load/176607", data="{}", headers=headers).json()["geojson"]["features"]
+    raw_json = requests.post("https://cvetrends.com/api/cves/24hrs", data="{}", headers=headers).json()
 
-    for entry in raw_json:
-        entry_list.append(entry["properties"])
-
-    return  entry_list
+    return raw_json
 
 class QueryClass:
     def __init__(self):
         self.data = ""
-        self.QueryTime = 10
+        self.QueryTime = 1800
         self.n = self.QueryTime
         parsed_yaml = manager.config.get_config()
         while True:
@@ -33,7 +29,7 @@ class QueryClass:
                 self.load_redis(parsed_yaml)
             time.sleep(1)
             self.n += 1
-            if not exists("ingest.MapHubCen4Rus.lck"):
+            if not exists("ingest.CveTrendsTweets.lck"):
                 break
 
     def load_redis(self, parsed_yaml):
@@ -47,19 +43,18 @@ class QueryClass:
             m = MongoClient(host=mongo_host, port=mongo_port)
             try:
                 existing_entries = []
-                mdb = m["ScrapeYard"]["MapHubCen4Rus"]
-                for x in mdb.find({}, {"title": json.loads(json.dumps(record))["title"]}):
+                mdb = m["ScrapeYard"]["CveTrendsTweets"]
+                for x in mdb.find({}, {"title": json.loads(json.dumps(record))["data"]["cve"]["tweets"]}):
                     existing_entries.append(x)
                 if len(existing_entries) == 0:
                     try:
                         r.rpush("data",
-                                '{"Module":"MapHubCen4Rus", "Data": ' + json.dumps(record) + ",\"TimeStamp\":\"" + str(
+                                '{"Module":"CveTrendsTweets", "Data": ' + json.dumps(record["data"]["cve"]["tweets"]) + ",\"TimeStamp\":\"" + str(
                                     time.time()) + '"}')
                     except:
-                        print(" MapHubCen4Rus: Unable to push to Redis stack")
+                        print(" CveTrendsTweets: Unable to push to Redis stack")
 
             except Exception as e:
-                # print(json_data)
                 print(e)
 
 
