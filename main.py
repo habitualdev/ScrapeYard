35bfd48482ce300ebd63103100947ae9fd28a8bf
habitual <habitual@habitual-media.com>
2022-03-28 19:25:11 -0400

    Fixed main

--- main.py
+++ main.py
@@ -18,11 +18,6 @@ def run_loop():
     parsed_yaml = manager.config.get_config()
     manager.config.run_modules(parsed_yaml)
     time.sleep(parsed_yaml["Manager"]["UpdatePeriod"])
-    files_in_directory = os.listdir(".")
-    filtered_files = [file for file in files_in_directory if file.endswith(".lck")]
-    for file in filtered_files:
-        path_to_file = os.path.join(".", file)
-        os.remove(path_to_file)
 
 
 if __name__ == "__main__":
